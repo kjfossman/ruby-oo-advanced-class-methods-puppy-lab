ded4692409b2ea42de48bdadef8049d11202188a
Kyle <kjfossman@gmail.com>
2021-02-11 06:26:56 -0900

    commit tree e7ac77f96d8fed2bfbabb1549d020231a2d1438c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+class Dog
+  
+    @@all = []
+
+    attr_reader :name 
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        prints = []
+        @@all.each do |x|
+            puts "#{x.name}"
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+
+    
+
+
+
+end
